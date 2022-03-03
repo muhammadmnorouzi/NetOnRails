46243e0670869ab91d0717030aaba31655a973db
Muhammad.M Norouzi <Muhammadmnorouzi@Outlook.com>
2022-03-03 22:09:56 +0330

    added bind and tests

---
+++ NetOnRails.Tests.Unit/BindTests.cs
@@ -0,0 +1,46 @@
+using System;
+using Shouldly;
+using Xunit;
+
+namespace NetOnRails.Tests.Unit
+{
+    public class BindTests : TestBase
+    {
+        [Fact]
+        public void BindOnResultSuccess()
+        {
+            //Given 
+            object obj = new object();
+            string expected = obj.GetHashCode().ToString();
+
+            Result<object, Exception> result = CreateSuccededResult<object, Exception>(obj);
+
+            //When
+            Result<string, Exception> binded = result
+                .Bind((data) => CreateSuccededResult<string, Exception>(
+                    data.GetHashCode().ToString()));
+
+            //Then
+            binded.Value.ShouldBe(expected);
+            binded.Error.ShouldBe(default(Exception));
+        }
+
+        [Fact]
+        public void BindOnResultFailure()
+        {
+            //Given 
+            Exception exc = new Exception();
+            Exception expected = exc;
+
+            Result<object, Exception> result = CreateFailedResult<object, Exception>(exc);
+
+            //When
+            Result<string, Exception> binded = result
+                .Bind((data) => CreateSuccededResult<string, Exception>(data.ToString()!));
+
+            //Then
+            binded.Value.ShouldBe(default(string));
+            binded.Error.ShouldBe(expected);
+        }
+    }
+}
---
+++ NetOnRails/RailsExtensions.Bind.cs
@@ -0,0 +1,14 @@
+namespace NetOnRails
+{
+    public static partial class RailsExtensions
+    {
+        public static Result<TDataOut, TError> Bind<TDataIn, TError, TDataOut>(
+            this Result<TDataIn, TError> result,
+            Func<TDataIn, Result<TDataOut, TError>> switchFunction)
+        {
+            return result.IsSuccess
+                ? switchFunction(result.Value)
+                : Result.Failure<TDataOut, TError>(result.Error);
+        }
+    }
+}
